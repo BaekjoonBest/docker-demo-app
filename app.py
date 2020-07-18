d9d5572dab97ffc32fdbb5be516f57e0eb3aa4ef
blurfx <its.blureffect@gmail.com>
2020-07-18 15:40:39 +0900

    feat: add echo view

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/echo/<text>')
+def echo(text):
+    return escape(text)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
